a91cf96a84bc5a40784fa1bdf4607eff3da73df8
emthy0 <40608590+emthy0@users.noreply.github.com>
2023-05-22 22:57:46 +0700

    PlatformIO setup

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,18 +1,70 @@
-#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266_SSL.h>
+#define BLYNK_TEMPLATE_ID "TMPL6rXyIyR1E"
+#define BLYNK_TEMPLATE_NAME "SMARTCURTAIN"
+#define BLYNK_AUTH_TOKEN "drFzungMkQBmEgqMe5r8Q8_wVuwsa_4Q"
+#define SSID "OnePlus 10 Pro 5G"
+#define PASS "aaaaaaaa"
 
-// put function declarations here:
-int myFunction(int, int);
+#define STM_RX D5
+#define STM_TX D4
+
+SoftwareSerial chat(STM_RX, STM_TX); // RX, TX
+
+String a;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
 
+  Serial.begin(9600);
+
+  chat.begin(4800);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN ,SSID,PASS);
+
+  delay(10);
+
+}
+int mode = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+Serial.println("Please wait Serial..");
+if(mode == 0){
+  while (a == "") {
+
+    chat.print("Q1"); // ส่งหัวข้อคำถาม ว่า Question1 ไปยัง Arduino
+    
+    a = chat.readString();  // อ่าน Serial และนำไปเก็บในตัวแปร A
+
+    Serial.print(a);
+
+    delay(100);
+
+    Serial.print(".Q1.");
+
+ 
+
+  }
+  a = "";
+  mode = 1;
+}
+else{
+  while (a == "") {
+
+    chat.print("Q2"); // ส่งหัวข้อคำถาม ว่า Question1 ไปยัง Arduino
+    
+    a = chat.readString();  // อ่าน Serial และนำไปเก็บในตัวแปร A
+
+    Serial.print(a);
+
+    delay(100);
+
+    Serial.print(".Q2.");
+
+ 
+
+  }
+  a = "";
+  mode = 0;
+}
 }
---
+++ stm32/src/main.cpp
@@ -0,0 +1,97 @@
+#include "DHT.h"
+#include "SparkFun_SGP30_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_SGP30
+#include <Wire.h>
+ 
+SGP30 mySensor; //create an object of the SGP30 class
+DHT dht;
+
+int a = 13;
+int b = 25;
+#include <SoftwareSerial.h>
+#define ESP_RX  PA8
+#define ESP_TX PA7_ALT1
+#define DHT_PIN PA0_ALT1
+// #define GAS_PIN = 
+SoftwareSerial chat(ESP_RX,ESP_TX); // RX, TX to NodeMCU
+int i;
+
+void setup()
+{
+  chat.begin(4800);
+  Serial.begin(9600);
+  //Serial.println();
+  //Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+
+  dht.setup(DHT_PIN); // data pin 2
+
+  //Wire.begin();
+  //Initialize sensor
+  //if (mySensor.begin(Wire) == false) {
+  //  Serial.println("No SGP30 Detected. Check connections.");
+  //  while (1);
+  //}
+  //Initializes sensor for air quality readings
+  //measureAirQuality should be called in one second increments after a call to 
+  //mySensor.initAirQuality();
+
+  
+}
+
+int counter = 0;
+void loop()
+{
+  //delay(dht.getMinimumSamplingPeriod());
+
+  //float humidity = dht.getHumidity(); // ดึงค่าความชื้น
+  //float temperature = dht.getTemperature(); // ดึงค่าอุณหภูมิ
+
+  //Serial.print(dht.getStatusString());
+  //Serial.print("\t");
+  //Serial.print(humidity, 1);
+  //Serial.print("\t\t");
+  //Serial.print(temperature, 1);
+  //Serial.print("\t\t");
+  //Serial.println(dht.toFahrenheit(temperature), 1);
+
+  //First fifteen readings will be
+  //CO2: 400 ppm  TVOC: 0 ppb
+  //delay(1000); //Wait 1 second
+  //measure CO2 and TVOC levels
+  //mySensor.measureAirQuality();
+  //Serial.print("CO2: ");
+  //Serial.print(mySensor.CO2);
+  //Serial.print(" ppm\tTVOC: ");
+  //Serial.print(mySensor.TVOC);
+  //Serial.println(" ppb");
+  //delay(1000);
+   if (chat.readString()){
+     if(chat.readString()== "Q1"){ //มีการถามคำถาม Question1 ส่งข้อมูลตัวแปร a ออกไป
+        delay(dht.getMinimumSamplingPeriod());
+
+        float humidity = dht.getHumidity(); // ดึงค่าความชื้น
+        float temperature = dht.getTemperature(); // ดึงค่าอุณหภูมิ
+
+        //chat.print(dht.getStatusString());
+        chat.print(humidity);
+        Serial.print(humidity);
+        //chat.print(temperature);
+        //chat.print(dht.toFahrenheit(temperature));
+
+     }
+
+     else if(chat.readString()== "Q2"){ //มีการถามคำถาม Question2 ส่งข้อมูลตัวแปร b ออกไป
+
+      chat.print(b);
+
+     }
+
+      Serial.print("Send = ");
+
+      Serial.println(i);
+
+  }
+
+  i++;
+  delay(1000);
+}
+
