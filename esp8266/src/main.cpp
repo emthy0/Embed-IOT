1a931ed3d138ef1e6c7ed1cd06f7b6cfbd99f681
emthy0 <ironpan10@hotmail.com>
2023-05-27 22:12:23 +0700

    This is an woiking uart

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,35 +1,16 @@
-#define BLYNK_PRINT Serial
-#define NO_GLOBAL_BLYNK true
-#include <BlynkSimpleEsp8266.h>
-#include <BlynkConfig.h>
-// #include <BlynkAdapter.h>
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include <Wire.h>
-#include <ESP8266WiFi.h>
-#include <BlynkConnector.h>
-#include <MasterCurtainController.h>
 
-void setup()
-{
-  Wire.begin();
-  Serial.begin(9600);
+SoftwareSerial NodeSerial(D2, D3); // RX |
+
+void setup() {
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
+  Serial.begin(115200);
+  NodeSerial.begin(4800);
 }
-
-void loop()
-{
-  Wire.requestFrom(8, 1);
-  byte a = Wire.read();
-  if (a == 1)
-  {
-    Serial.println("Master LED ON");
-  }
-  else
-  {
-    Serial.println("Master LED OFF");
-  }
-  Wire.beginTransmission(8);
-  Wire.write("Hola");
-  Wire.endTransmission();
-  delay(1000);
+void loop() {
+  NodeSerial.print(10);
+  NodeSerial.print("\n");
+  delay(100);
 }
--- stm32/src/main.cpp
+++ stm32/src/main.cpp
@@ -1,56 +1,23 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
-#include <Wire.h>  
-#include "DHT.h"
-#include "SparkFun_SGP30_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_SGP30
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include <BuzzerController.h>
-#include <CurtainController.h>
-#include <SensorController.h>
-#include <MotorController.h>
-#include <LEDController.h>
-#include <PinConfig.h>
-#include <GlobalVar.h>
-#include <SlaveChatController.h>
-#include <SlaveCurtainController.h>
-#include <DHTController.h>
-#include <MQController.h>
-#include <SGPController.h>
-#include <LDRController.h>
-
-String data = "";
-
-void requestEvent()                         
-  {
-  Wire.write(1);                           
-}
- 
-
-void receiveEvent(int howMany)
-{
-  Serial.println("receiveEvent");
-  data = "";
-  while( Wire.available()){
-    data += (char)Wire.read();
-    Serial.println(data);
-  }
-}
-
+#include <HardwareSerial.h>
+HardwareSerial Serial6(PA_12, PA_11); // RX | TX
 
 void setup()
 {
-  Serial.begin(9600); 
-  Wire.begin(8);
-  Wire.onRequest(requestEvent);
-  Wire.onReceive(receiveEvent);
-  
+  Serial.begin(9600);
+  Serial6.begin(4800);
 }
-
 
 void loop()
 {
-  // 
-  // requestEvent()
-  // delay(1000);
+  while (Serial6.available() > 0)
+  {
+    float val = Serial6.parseFloat();
+    if (Serial6.read() == '\n')
+    {
+      Serial.println(val);
+    }
+  }
+  delay(100);
 }
